c6d0a70d1e08556568d9bd3dc203fdc6a97af19f
Mahmoud Ismail <maism@hops.io>
2019-04-24 15:12:46 +0200

    reapply HDFS-6565 for XAttrs

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/web/TestJsonUtil.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/web/TestJsonUtil.java
@@ -35,7 +35,6 @@ import org.codehaus.jackson.map.ObjectReader;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -50,7 +49,6 @@ import static org.apache.hadoop.fs.permission.FsAction.ALL;
 import static org.apache.hadoop.fs.permission.FsAction.READ_EXECUTE;
 import static org.apache.hadoop.fs.permission.FsAction.READ_WRITE;
 import org.apache.hadoop.hdfs.protocol.DatanodeInfo;
-import org.mortbay.util.ajax.JSON;
 
 import static org.apache.hadoop.hdfs.server.namenode.AclTestHelpers.aclEntry;
 
@@ -205,7 +203,8 @@ public class TestJsonUtil {
     String jsonString =
         "{\"XAttrs\":[{\"name\":\"user.a1\",\"value\":\"0x313233\"}," +
         "{\"name\":\"user.a2\",\"value\":\"0x313131\"}]}";
-    Map<?, ?> json = (Map<?, ?>) JSON.parse(jsonString);
+    ObjectReader reader = new ObjectMapper().reader(Map.class);
+    Map<?, ?> json = reader.readValue(jsonString);
     XAttr xAttr1 = (new XAttr.Builder()).setNameSpace(XAttr.NameSpace.USER).
         setName("a1").setValue(XAttrCodec.decodeValue("0x313233")).build();
     XAttr xAttr2 = (new XAttr.Builder()).setNameSpace(XAttr.NameSpace.USER).
